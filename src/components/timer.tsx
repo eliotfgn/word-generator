b5eb0e59c9ec218b5b30e75f3c79c0b72b0c95b3
Eliot Fagnon <efagnon2002@gmail.com>
2023-02-07 20:02:06 +0100

    make progress bar

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -11,6 +11,14 @@ function Timer() {
     useEffect(() => {
         let interval = setInterval(() => {
             setSec(_sec => {
+                let percentage = _sec / 45;
+                let progressBar = document.getElementById("progress");
+                if (progressBar) {
+                    progressBar.style.background = `conic-gradient(
+                        #4d5df9 ${percentage * 360}deg,
+                        #b44040 0deg
+                    )`
+                }
                 if (_sec === 0) {
                     let index = Math.floor(Math.random() * words.length);
                     setWord(words[index]);
@@ -24,7 +32,7 @@ function Timer() {
 
 
     return (
-        <div className="timer">
+        <div className="timer" id="progress">
             <p className="bg-[#41410142] px-4 py-2 rounded-md absolute top-1/2">
                 {word}               
             </p>
